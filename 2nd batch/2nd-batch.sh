ac513a61d3c7e9c452c21c998a3ddd2852b7e8ef
Eduardo Ribeiro <1600820@estudante.uab.pt>
2023-08-22 15:32:06 +0100

    Added 2nd batch

---
+++ 2nd batch/2nd-batch.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# In this batch we compare the impact of the number of processes
+# We run all four prototypes in a defined dataset c2_a8M_o2k_i100_u40_p26.h5
+# to compare the effect on runtime for different number of processes
+#
+# Goin for 1, 2, 3, 6, 12, 24, 48, 96, 192 processes
+# We have results for one process in batch #1
+#
+# c2_a8M_o2k_i100_u40_p26.h5
+
+# Usage: run.sh N_TASKS N_NODES N_TASKS_PER_NODE PROTOTYPE DATASET_FILE REMOVE_DATASET QUEUE_SLURM REMOVE_SLURM
+
+source "../base/base.sh"
+
+processes=(2 3 6 12 24 48 96 192)
+
+#P2: P1 and P2 must be run individually, because they use the disk
+mkdir -p P2
+cd P2
+
+for p in "${processes[@]}"
+do
+	bash "$SLURM_BASE_DIR/run.sh" "$p" 1 96 P2 c2_a8M_o2k_i100_u40_p26.h5 YES NO NO
+done
+
+cd ..
+
+#P3:
+mkdir -p P3
+cd P3
+
+for p in "${processes[@]}"
+do
+	bash "$SLURM_BASE_DIR/run.sh" "$p" 1 96 P3 c2_a8M_o2k_i100_u40_p26.h5 YES NO NO
+done
+
+cd ..
+
+#P4:
+mkdir -p P4
+cd P4
+
+for p in "${processes[@]}"
+do
+	bash "$SLURM_BASE_DIR/run.sh" "$p" 1 96 P4 c2_a8M_o2k_i100_u40_p26.h5 YES NO NO
+done
+
+cd ..
